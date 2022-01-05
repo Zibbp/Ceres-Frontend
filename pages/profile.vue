5c72e81368639b755e48abace08d2f3202fa1ce3
Zibbp <developerzippy@gmail.com>
2022-01-05 11:57:32 -0600

    Change password feature

--- pages/profile.vue
+++ pages/profile.vue
@@ -40,6 +40,71 @@
                   class="block w-full px-4 py-2 mt-2 text-gray-700 dark:text-gray-300 placeholder-gray-500 bg-white border rounded-md dark:bg-neutral-700 dark:border-gray-600 dark:placeholder-gray-400 focus:border-dark-purple-500 ring-dark-purple-600 dark:focus:border-dark-purple-600 focus:outline-none focus:ring"
                   type="text"
                   aria-label="Webhook"
+                  placeholder="https://discord.com/api/webhooks/123456789/feweopifsdgjklmdffikwo"
+                  required
+                />
+              </div>
+
+              <div class="flex items-center justify-between mt-4">
+                <a
+                  href="#"
+                  class="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500"
+                ></a>
+
+                <button
+                  class="px-4 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none"
+                  type="submit"
+                >
+                  Update
+                </button>
+              </div>
+            </form>
+            <h3 class="text-xl mt-1 text-gray-700 dark:text-gray-100 font-bold">
+              Change Password
+            </h3>
+            <form @submit.prevent="updatePassword()">
+              <div class="w-full mt-4">
+                <label
+                  for="old"
+                  class="block text-sm font-medium text-gray-700 dark:text-gray-200"
+                  >Old Password</label
+                >
+
+                <input
+                  v-model="password.old"
+                  class="block w-full px-4 py-2 mt-2 text-gray-700 dark:text-gray-300 placeholder-gray-500 bg-white border rounded-md dark:bg-neutral-700 dark:border-gray-600 dark:placeholder-gray-400 focus:border-dark-purple-500 ring-dark-purple-600 dark:focus:border-dark-purple-600 focus:outline-none focus:ring"
+                  type="password"
+                  id="old"
+                  aria-label="old password"
+                  placeholder="Old Password"
+                  required
+                />
+                <label
+                  for="new"
+                  class="block text-sm font-medium mt-1 text-gray-700 dark:text-gray-200"
+                  >New Password</label
+                >
+                <input
+                  v-model="password.new"
+                  class="block w-full px-4 py-2 mt-2 text-gray-700 dark:text-gray-300 placeholder-gray-500 bg-white border rounded-md dark:bg-neutral-700 dark:border-gray-600 dark:placeholder-gray-400 focus:border-dark-purple-500 ring-dark-purple-600 dark:focus:border-dark-purple-600 focus:outline-none focus:ring"
+                  type="password"
+                  id="new"
+                  aria-label="new password"
+                  placeholder="New Password"
+                  required
+                />
+                <label
+                  for="confirm"
+                  class="block text-sm font-medium mt-1 text-gray-700 dark:text-gray-200"
+                  >Confirm Password</label
+                >
+                <input
+                  v-model="password.confirm"
+                  class="block w-full px-4 py-2 mt-2 text-gray-700 dark:text-gray-300 placeholder-gray-500 bg-white border rounded-md dark:bg-neutral-700 dark:border-gray-600 dark:placeholder-gray-400 focus:border-dark-purple-500 ring-dark-purple-600 dark:focus:border-dark-purple-600 focus:outline-none focus:ring"
+                  type="password"
+                  id="confirm"
+                  aria-label="Confirm new password"
+                  placeholder="Confirm new password"
                   required
                 />
               </div>
@@ -76,6 +141,11 @@ export default {
   data() {
     return {
       user: {},
+      password: {
+        old: '',
+        new: '',
+        confirm: '',
+      },
     }
   },
   async mounted() {
@@ -94,6 +164,27 @@ export default {
         this.$toast.success('User updated')
       } catch (error) {
         this.$toast.error('Error updating user')
+      }
+    },
+    async updatePassword() {
+      if (this.password.new !== this.password.confirm) {
+        this.$toast.error('New passwords do not match')
+        return
+      }
+      try {
+        await this.$axios.patch(
+          `${this.$config.apiURL}/v1/users/${this.$auth.user.id}/password`,
+          {
+            old: this.password.old,
+            new: this.password.new,
+            confirm: this.password.confirm,
+          }
+        )
+        this.$toast.success('Password updated, please log back in')
+        await this.$auth.logout()
+      } catch (error) {
+        console.log(error.response.data.message)
+        this.$toast.error(error.response.data.message)
       }
     },
   },
