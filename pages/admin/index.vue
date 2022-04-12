6d87522fa747dd07ec4e60fd315160ffa83928d0
Zibbp <developerzippy@gmail.com>
2022-04-12 14:59:15 -0500

    feat: UI pages for live channels
    Add UI elements for live archiving feature

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -111,6 +111,33 @@
                 </a></div
             ></nuxt-link>
           </div>
+          <div class="p-4 lg:w-1/3">
+            <nuxt-link to="/admin/live">
+              <div
+                class="bg-gray-100 dark:bg-neutral-800 bg-opacity-75 px-8 pt-8 pb-8 rounded-lg overflow-hidden text-center relative"
+              >
+                <h1
+                  class="title-font sm:text-2xl text-xl font-medium text-gray-900 dark:text-gray-100 mb-3"
+                >
+                  Manage Live Channels
+                </h1>
+                <a class="text-indigo-500 inline-flex items-center"
+                  >Live
+                  <svg
+                    class="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a></div
+            ></nuxt-link>
+          </div>
         </div>
       </div>
     </section>
